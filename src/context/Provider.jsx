545fbc520bc0e4aba45d18836d9a73a6f67523f4
Alberto <betim009@gmail.com>
2024-09-21 17:06:45 -0300

    update

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,13 +1,20 @@
-import { Children, useState } from "react";
+import { useEffect, useState } from "react";
 import Context from "./Context";
+import PropTypes from "prop-types";
 
 export default function Provider({ children }) {
   const [anyValue, setAnyValue] = useState("");
+
+  useEffect(() => {}, []);
 
   const contextValue = {
     anyValue,
     setAnyValue,
   };
 
-  return <Context.Provider value={contextValue}>{Children}</Context.Provider>;
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 }
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
